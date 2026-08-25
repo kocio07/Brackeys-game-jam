f1f83f99eb82e21ce2569e08e7198ffb699c6f12
wuff <kacper5555544444@gmail.com>
2026-08-25 23:50:38 +0200

    Added template of enemy and collision and update gitignore (i broke gitignore :c)

---
+++ nowy-projekt-gry/skrypty/wrog_1.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+
+func _on_death_body_entered(body: Node2D) -> void:
+	if body.is_in_group("gracz"):
+		respawn_gracz(body)
+		
+		
+func respawn_gracz(body: CharacterBody2D) -> void:
+	if Global.checkpoint_pos != Vector2(-999, -999):
+		body.global_position = Global.checkpoint_pos
+		body.velocity = Vector2.ZERO
+	else:
+		get_tree().reload_current_scene()
