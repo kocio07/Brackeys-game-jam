2a422e42f5beeef6b0b39fe64d20c8c11ec83501
wuff <kacper5555544444@gmail.com>
2026-08-26 23:39:40 +0200

    Added movement to enemie 1

--- nowy-projekt-gry/skrypty/wrog_1.gd
+++ nowy-projekt-gry/skrypty/wrog_1.gd
@@ -1,5 +1,34 @@
 extends CharacterBody2D
 
+@export_group("Movement")
+@export var speed: float = 80.0
+@export var gravity: float = 980.0
+@export var start_facing_left: bool = false
+
+var direction: int = 1
+
+@onready var edge_detector: RayCast2D = $EdgeDetector
+
+func _ready() -> void:
+	if start_facing_left:
+		direction = -1
+		scale.x = -abs(scale.x)
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		if is_on_wall() or not edge_detector.is_colliding():
+			flip()
+	
+	velocity.x = direction * speed
+	move_and_slide()
+
+func flip() -> void:
+	direction *= -1
+	scale.x *= -1
+	edge_detector.force_raycast_update()
+
 
 func _on_death_body_entered(body: Node2D) -> void:
 	if body.is_in_group("gracz"):
@@ -13,3 +42,10 @@ func respawn_gracz(body: CharacterBody2D) -> void:
 		body.velocity = Vector2.ZERO
 	else:
 		get_tree().reload_current_scene()
+
+
+func _on_weak_body_entered(body: Node2D) -> void:
+	if body.is_in_group("gracz"):
+		if body.has_method("bounce"):
+			body.bounce()
+		queue_free()
